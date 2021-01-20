56fddca3aca4585584f7efa918cf575b2521d70c
Jacob Moberly <jacobmoberly13@gmail.com>
2021-01-20 07:27:34 +0000

    Added necessary files and got initial bug testing included

---
+++ TodoList.h
@@ -0,0 +1,54 @@
+#ifndef TODO_LIST_H_
+#define TODO_LIST_H_
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "TodoListInterface.h"
+
+using namespace std;
+
+class TodoList: public TodoListInterface {
+public:
+    TodoList() {
+        cout << "In Constructor" << endl;
+				ifstream in
+    }
+
+    virtual ~TodoList() {
+        cout << "In Destructor" << endl;
+    }
+    /*
+     * Adds an item to the todo list with the data specified by the string "_duedate" and the task specified by "_task"
+    */
+
+    virtual void add(string _duedate, string _task) {
+        cout << "In Add" << endl;
+    }
+    /*
+     * Removes an item from the todo list with the specified task name
+     *
+     * Returns 1 if it removes an item, 0 otherwise
+     */
+
+    virtual int remove(string _task) {
+        cout << "In Remove" << endl;
+    }
+    /*
+     * Prints out the full todo list to the console
+     */
+
+    virtual void printTodoList() {
+        cout << "In Print List" << endl;
+    }
+    /*
+     * Prints out all items of a todo list with a particular due date (specified by _duedate)
+    */
+
+    virtual void printDaysTasks(string _date) {
+        cout << "In Print Days" << endl;
+    }
+};
+
+
+#endif //TODO_LIST_H_
--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include "TodoList.h"
 
-int main() {
-  std::cout << "Hello World!\n";
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    TodoList myList;
+    for(int i = 0; i < argc; i++) {
+        cout << "argv[" << i << "]=" << argv[i] << endl;
+    }
+    string firstarg = argv[1];
+    if(firstarg.compare("add")==0) {
+        cout << "Doing an add" << endl;
+        string date = argv[2];
+        string task = argv[3];
+        cout << "date " << date << " task " << task << endl;
+        myList.add(date, task);
+    }
 }
